24ae3b5ebeeb335c275f76a3a991475fda24db6d
Brendan Broadstreet <brendanbroadstreet@macbook-pro-br.attlocal.net>
2020-02-16 14:57:41 -0500

    update form element types

--- src/component/auth/SignUp.js
+++ src/component/auth/SignUp.js
@@ -44,7 +44,7 @@ return (
         <FormGroup>
             <Label htmlFor='password'></Label>
             <Input onChange={(e) => setPassword(e.target.value)}
-            type='password' 
+            type='new-password' 
             placeholder= 'password' 
             name='password' 
             placeholder='Password'
